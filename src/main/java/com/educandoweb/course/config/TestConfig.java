39aebdb55b8e73bb74ae091f85265069389e3ce6
GabeZape <gabrielzapellaro@gmail.com>
2023-10-05 11:44:03 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -5,13 +5,11 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 
 @Configuration
-@Profile("test")
 public class TestConfig implements CommandLineRunner {
 
 	@Autowired
@@ -20,8 +18,8 @@ public class TestConfig implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u1 = new User(null, "Tonia Black", "tonia@gmail.com", "96666666", "623456"); 
+		User u2 = new User(null, "Chris Rock", "chris@gmail.com", "95555555", "523456");
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 	}
